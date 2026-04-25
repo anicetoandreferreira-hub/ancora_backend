ac21b9d9067aec225861c9b747bcfa494984466e
anicetoandreferreira-hub <anicetoandreferreira@gmail.com>
2026-04-25 22:34:46 +0100

    Update websocket.py

--- backend/routes/websocket.py
+++ backend/routes/websocket.py
@@ -1,15 +1,8 @@
-# routes/websocket.py
 from flask_socketio import SocketIO
 
 socketio = SocketIO(
-    cors_allowed_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1"
-    ],
-    cookie='io',  # MUDOU: True -> 'io' ou remove a linha toda
-    async_mode='threading',
+    cors_allowed_origins="*",
+    async_mode="eventlet",
     logger=True,
     engineio_logger=True
 )
