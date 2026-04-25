c1a7bb741615503b021ffed8013371cd504d1dc9
anicetoandreferreira-hub <anicetoandreferreira@gmail.com>
2026-04-26 00:36:18 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -51,10 +51,10 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {
     "origins": [
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5173"
         "https://ancora-ecommerce.netlify.app"
     ],
-    "supports_credentials": True,
+    "supports_credentials"=True,
     "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     "allow_headers": ["Content-Type", "Authorization"]
 }})
