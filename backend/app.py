a356a71a8e61cda6e7d6536557f3b3f57369e050
anicetoandreferreira-hub <anicetoandreferreira@gmail.com>
2026-04-25 22:39:57 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
 # app.py
 import eventlet
+eventlet.monkey_patch()
 from dotenv import load_dotenv
 import os
 from flask import Flask
 from flask_cors import CORS
 from models.database import db
 from flask_talisman import Talisman
-eventlet.monkey_patch()
 load_dotenv()
 
 # 🔥 ADICIONADO (lógica de deploy)
