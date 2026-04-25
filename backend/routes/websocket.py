5376c101bd329bf5784180d2b119605c23ab3c7d
anicetoandreferreira-hub <anicetoandreferreira@gmail.com>
2026-04-26 00:26:45 +0100

    Update websocket.py

--- backend/routes/websocket.py
+++ backend/routes/websocket.py
@@ -2,14 +2,10 @@ from flask_socketio import SocketIO
 
 socketio = SocketIO(
     cors_allowed_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://ancora-ecommerce.netlify.app/"
+        "https://ancora-ecommerce.netlify.app"
     ],
     async_mode="eventlet",
     logger=True,
     engineio_logger=True,
-    ping_timeout=60,
-    ping_interval=25,
-    allow_upgrades=True
+    manage_session=False
 )
