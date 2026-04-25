88ab56720bab311ee7f2196918c040aff486846c
anicetoandreferreira-hub <anicetoandreferreira@gmail.com>
2026-04-26 00:19:09 +0100

    Update websocket.py

--- backend/routes/websocket.py
+++ backend/routes/websocket.py
@@ -4,10 +4,12 @@ socketio = SocketIO(
     cors_allowed_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://ancora-oficial-ao.netlify.app",
-        "https://ancora-oficial-ao.netlify.app/"
+        "https://ancora-ecommerce.netlify.app/"
     ],
     async_mode="eventlet",
     logger=True,
-    engineio_logger=True
+    engineio_logger=True,
+    ping_timeout=60,
+    ping_interval=25,
+    allow_upgrades=True
 )
