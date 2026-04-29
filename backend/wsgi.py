f08913f95ed03ade14c153a68aab00904730424c
anicetoandreferreira-hub <anicetoandreferreira@gmail.com>
2026-04-30 00:37:37 +0100

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,9 +1,9 @@
-# wsgi.py  ← Este será o arquivo principal para o Render
-
+# wsgi.py   ← Este é o mais importante
 import eventlet
-eventlet.monkey_patch()   # ← TEM que ser a PRIMEIRA coisa
+eventlet.monkey_patch()   # ← Tem que ser a PRIMEIRA coisa a ser executada
 
-from app import app, socketio   # Agora importamos o app depois do patch
+from app import app, socketio
 
+# Para rodar localmente (opcional)
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000, debug=False)
