b481e37fa4fc581ad3712f7b5df6220b1cb74d28
anicetoandreferreira-hub <anicetoandreferreira@gmail.com>
2026-04-30 01:40:33 +0100

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,10 +1,10 @@
-
 # wsgi.py
 import gevent.monkey
-gevent.monkey.patch_all()     # ← Substitui o eventlet.monkey_patch
+gevent.monkey.patch_all()   # Deve ser a primeira coisa a ser executada
 
 from app import app
 
+# Para rodar localmente (opcional)
 if __name__ == "__main__":
     from app import socketio
     socketio.run(app, host="0.0.0.0", port=5000, debug=False)
